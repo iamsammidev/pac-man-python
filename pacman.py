7e16344804ed0134702c3c15bcebd9b8d43f3311
Samir <sami.mir.aner@gmail.com>
2023-01-13 15:24:00 +0500

    Drawing and Animating the player onto the screen

--- pacman.py
+++ pacman.py
@@ -14,9 +14,16 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
 color = 'blue'
 PI = math.pi
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
+player_x = 450
+player_y = 663
+direction = 0
+counter = 0
 
 
-def draw_board(lvl):
+def draw_board():
     num1 = ((HEIGHT - 50) // 32)
     num2 = (WIDTH // 30)
     for i in range(len(level)):
@@ -44,12 +51,29 @@ def draw_board(lvl):
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 
+def draw_player():
+    # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+    if direction == 0:
+        screen.blit(player_images[counter // 5], (player_x, player_y))
+    elif direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+    elif direction == 2:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], 90), (player_x, player_y))
+    elif direction == 3:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], 270), (player_x, player_y))
+
 
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+    else:
+        counter = 0
+
     screen.fill('black')
-    draw_board(level)
+    draw_board()
+    draw_player()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
